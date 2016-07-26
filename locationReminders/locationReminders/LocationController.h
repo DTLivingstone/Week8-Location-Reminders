7c9ef3fe706c8059652b3b3269ef89c7520535fe
David Livingstone <david@david.tl>
2016-07-26 10:52:25 -0700

    set up location delegates

---
+++ locationReminders/locationReminders/LocationController.h
@@ -0,0 +1,27 @@
+//
+//  LocationController.h
+//  locationReminders
+//
+//  Created by David Livingstone on 7/26/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@import CoreLocation;
+
+@protocol LocaitonControllerDelegate <NSObject>
+
+- (void)locationControllerDidUpdateLocation:(CLLocation *)location;
+
+@end
+
+@interface LocationController : NSObject
+
+@property(strong, nonatomic) CLLocationManager *locationManager;
+@property(strong, nonatomic) CLLocation *location;
+
+@property(weak, nonatomic) id delegate;
+
+
+@end
